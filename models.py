999569d8e3c876d5e7d96b19f2112c5a01aa0828
Alyssa Harris <amh302918@gmail.com>
2022-03-01 16:08:49 -0500

    created feedback model for user pages

--- models.py
+++ models.py
@@ -51,4 +51,18 @@ class User(db.Model):
         else:
             return False
 
+    class Feedback(db.Model):
+        """Feedback."""
+
+    __tablename__ = "feedback"
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    username = db.Column(
+        db.String(20),
+        db.ForeignKey('users.username'),
+        nullable=False,
+    )
+
 
