743da4bbf627aa52c870b1523cb2602db6f4f55d
Alyssa Harris <amh302918@gmail.com>
2022-03-01 17:29:11 -0500

    added login and logout routes

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, session, get_flashed_messages
+from flask import Flask, render_template, redirect, session, flash
 from flask_debugtoolbar import DebugToolbarExtension
 from models import User, connect_db, db
 from forms import LoginForm, RegisterForm
@@ -22,8 +22,10 @@ def home_page():
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     "Registers user and produces form to handle registration"
+    if "username" in session:
+        return redirect(f"/users/{session['username']}")
     form = RegisterForm()
-
+    
     if form.validate_on_submit():
         username = form.username.data
         password = form.password.data
@@ -34,8 +36,39 @@ def register():
         user = User.register(username, password, first_name, last_name, email)
 
         db.session.commit()
-        return redirect("/show")
+        session['username'] = user.username
+        flash('Welcome! You have successfully created your account')
+        return redirect(f"/users/{user.username}")
         
     else:
         return render_template("users/register.html", form=form)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    """Produce login form and handle login."""
+
+    if "username" in session:
+        return redirect(f"/users/{session['username']}")
+
+    form = LoginForm()
+
+    if form.validate_on_submit():
+        username = form.username.data
+        password = form.password.data
+
+        user = User.authenticate(username, password)
+        if user:
+            session['username'] = user.username
+            return redirect(f"/users/{user.username}")
+        else:
+            form.username.errors = ["Invalid username/password."]
+            return render_template("users/login.html", form=form)
+
+    return render_template("users/login.html", form=form)
  
+@app.route("/logout")
+def logout():
+    """Logout route for ussers."""
+
+    session.pop("username")
+    return redirect("/login")
